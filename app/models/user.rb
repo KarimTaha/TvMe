1c5761c309131935b9c94d9a23b29c640d6883d1
karim taha <karimtaha7000@gmail.com>
2015-11-16 01:10:35 +0200

    added attribute accessors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+	attr_accessor :f_name
+	attr_accessor :l_name
+	attr_accessor :city
+	attr_accessor :country
+	attr_accessor :date_of_birth
+	attr_accessor :gender
 	acts_as_followable
     acts_as_follower
 end
